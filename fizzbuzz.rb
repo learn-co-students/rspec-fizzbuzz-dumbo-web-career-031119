535e9b9dfdf547216a4bbef18782355fee35a640
Kate F <follett.katie@gmail.com>
2019-02-25 06:32:07 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,29 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+require 'pry'
+
+# Takes the argument of a number0 and number1. 
+# what:
+# Takes an argument of number0 and modulo number1 to equal 0.
+# why: A number is divisible when there no remainders.
+
+# Whether number 0 is divisible by number 1.
+
+# Inputs: It takes in 2 numbers
+# Outputs: A boolean saying whether number0 is divisible by number1.
+def divisible?(number0, number1)
+  (number0 % number1) == 0
+end
+
+# Inputs: a number
+# Outputs:
+#   Returns a string when number0 is divisible by 3 and 5.
+def fizzbuzz(number)
+  # (number % 3) == 0
+  if divisible?(number, 3)
+    return "Fizz"
+  # (number % 5) == 0
+  elsif divisble?(number, 5)
+    return "Buzz"
+  elsif (number % ) == 0 
+    return "Fizzbuzz"
+  end
+end
